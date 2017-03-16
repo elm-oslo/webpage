e7b9c3722005c0b804d1a8c571679106f5da3dda
Stian Veum Møllersen <stian.mollersen@sparebank1.no>
2017-03-16 10:35:13 +0100

    Working MVP

--- site/script.js
+++ site/script.js
@@ -1,135 +1,97 @@
-var c = document.querySelector('#backdrop');
-var ctx = c.getContext('2d');
+const canvas = document.querySelector('#backdrop');
+const defs = canvas.querySelector('defs');
+const createSVGElement = document.createElementNS.bind(document,  'http://www.w3.org/2000/svg');
+const setAttributes = (el, map) => Object.keys(map).forEach(k => el.setAttribute(k, map[k]));
+const setXlink = (use, id) => use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `#${id}`);
 
-var HEIGHT = window.innerHeight / 2;
-var WIDTH = window.innerWidth;
-c.setAttribute('height', HEIGHT);
-c.setAttribute('width', WIDTH);
+const cos = Math.cos;
+const sin = Math.sin;
 
-/*
-  [1 0 -x]
-  [0 1 -y]
-  [0 0 1]
-x
-  [cos sin 0]
-  [-sin cos 0]
-  [0 0 1]
-=
-  //
-  // [cos sin -x]
-  // [-sin cos -y]
-  // [0 0 1]
-  //
-x
-  [1 0 x]
-  [0 1 y]
-  [0 0 1]
-=
-  //
-  // [cos sin (xcos ysin -x)]
-  // [-sin cos (-xsin ycos -y)]
-  // [0 0 1]
-  //
-x
-  [1 0 tx]
-  [0 1 0]
-  [0 0 1]
-=
-  //
-  // [cos sin (txcos (xcos ysin -x))]
-  // [-sin cos (-txsin (-xsin ycos -y))]
-  // [0 0 1]
-  //
-x
-  [x]
-  [y]
-  [1]
-=
-  //
-  // [xcos ysin (txcos (xcos ysin -x))]
-  // [-xsin ycos (-txsin (-xsin ycos -y))]
-  // [1]
-  //
+//internal SVG coordinate system
+const HEIGHT = 500;
+const WIDTH = 1000;
 
-*/
+const gradientColors = [
+  ['#9012ef', '#ce6bdb'],
+  ['#f0ad00', '#ce6bdb'],
+  ['#60b4cc', '#8581b0'],
+  ['#7fd13b', '#8581b0'],
+  ['#5a6378', '#ce6bdb']
+];
 
+function createGradient([c1, c2], i) {
+  const el = createSVGElement('radialGradient');
+  const s1 = createSVGElement('stop');
+  const s2 = createSVGElement('stop');
 
-function transform(tx, r, [x, y]) {
-  return [
-    2*x*Math.cos(r) + 2*y*Math.sin(r) + tx*Math.cos(r) - x,
-    -2*x*Math.sin(r) + 2*y*Math.cos(r) - tx*Math.sin(r) - y
-  ];
+  setAttributes(el, {
+    id: `gradient${i}`,
+    cx: '50%',
+    cy: '50%',
+    r: '75%',
+    fx: '0%',
+    fy: '0%'
+  });
+
+  setAttributes(s1, {
+    'stop-color': c1,
+    offset: '0%'
+  });
+
+  setAttributes(s2, {
+    'stop-color': c2,
+    offset: '100%'
+  });
+
+  el.appendChild(s1);
+  el.appendChild(s2);
+
+  return el;
 }
 
-function shape({vertices, initialR, vx, vr, style: {stroke, fill}}) {
-  let state = {
-    x: -200,
-    r: initialR
-  }
-
-  function draw() {
-    ctx.fillStyle = fill;
-    ctx.strokeStyle = stroke;
-    ctx.beginPath();
-    vertices
-      .map(transform.bind(null, state.x, state.r))
-      .forEach(function([x, y], i) {
-        if(i === 0) { ctx.moveTo(x, y); }
-        else { ctx.lineTo(x, y); }
-      });
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-  }
-
-  return function update() {
-    draw();
-
-    state = {
-      x: state.x + vx,
-      r: state.r + vr
-    }
-
-    if(state.x > WIDTH + 400) {
-      state.x = -200;
-    }
-
-    if(state.r > 2*Math.PI) {
-      state.r = state.r - 2*Math.PI;
-    }
-
-  }
-}
+const gradients = gradientColors
+  .map(createGradient)
+  .forEach(el => defs.appendChild(el));
 
 function rand2(a, b) {
-  return b + Math.random() * (a - b);
+  return a + Math.random() * (b - a);
 }
 
 function randomSquare() {
   const minWidth = 50;
-  const x0 = 0;
-  const y0 = rand2(minWidth, HEIGHT - minWidth);
+  const x = rand2(-WIDTH, -minWidth*4);;
+  const y = rand2(minWidth, HEIGHT - minWidth*4);
   const w = rand2(minWidth, minWidth * 4);
+  const stroke = 'none';
+  const fill = `url(#gradient${Math.floor(rand2(0, 5))})`;
+  const className = 'shape';
 
-  return [
-    [x0, y0],
-    [x0 + w, y0],
-    [x0 + w, y0 + w],
-    [x0, y0 + w]
-  ]
+  const el = createSVGElement('rect');
+  setAttributes(el, {
+    x, y, stroke, fill,
+    width: w, height: w,
+    class: className
+  });
+  return el;
 }
 
 function randomTriangle() {
   const minWidth = 25;
-  const x0 = 0;
-  const y0 = rand2(minWidth, HEIGHT - minWidth);
+  const x = rand2(-WIDTH, -minWidth*4);
+  const y = rand2(minWidth, HEIGHT - minWidth*4);
   const w = rand2(minWidth, minWidth * 4);
+  const stroke = 'none';
+  const fill = `url(#gradient${Math.floor(rand2(0, 5))})`;
+  const className = 'shape';
 
-  return [
-    [x0, y0],
-    [x0 + w, y0 + w],
-    [x0, y0 + 2*w]
-  ]
+  const el = createSVGElement('polygon');
+  setAttributes(el, {
+    stroke, fill,
+    points: `${x},${y} ${x + w},${y + w} ${x},${y + 2*w}`,
+    class: className
+  });
+
+  return el;
 }
 
 const shapes = [
@@ -145,16 +107,34 @@ const shapes = [
   randomTriangle()
 ];
 
-const initialized = shapes
-  .map(function(vertices) {
-    return shape({
-      vertices,
-      initialR: rand2(0, Math.PI),
-      vx: rand2(1, 5),
-      vr: rand2(0, Math.PI * 0.01),
-      style: {stroke: '#9012ef', fill: '#ce6bdb'}
-    });
-  });
+function initialize(el) {
+  canvas.appendChild(el);
+
+  const vx = rand2(0.5, 5);
+  const vr = rand2(0.36, 1.8);
+  const initialR = rand2(0, 180);
+  const initialX = -200;
+
+  let state = {
+    x: initialX,
+    r: initialR
+  }
+
+  return function update() {
+    state.x = state.x + vx;
+    if(state.x > WIDTH*2) {
+      state.x = 0;
+    }
+
+    state.r = state.r + vr;
+    if(state.r > 360) {
+      state.r = state.r - 360;
+    }
+    el.setAttribute('style', `transform: translateX(${state.x}px) rotate(${state.r}deg);`);
+  }
+}
+
+const initialized = shapes.map(initialize);
 
 let t0 = 0;
 let rafId = 0;
@@ -163,9 +143,9 @@ function main(t) {
 
   rafID = requestAnimationFrame(main);
   if(t0 && delta < (60/30) * 16 + 1) { return; }
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0,0,WIDTH,HEIGHT);
+
   initialized.forEach(i => i());
+
   t0 = t;
 }
 
