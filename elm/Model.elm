90c9e0d2e58455d023e208a67ac7009fd69e1e12
Erik Wendel <erik.wendel@bekk.no>
2017-06-10 12:07:28 +0200

    More program updates

--- elm/Model.elm
+++ elm/Model.elm
@@ -264,47 +264,47 @@ scheduleEntries =
         { title = "Everything in Elm"
         , abstract = "During my time at my own company, at NoRedInk, and in the open source community, I like to think I proved that anything that could be written in Javascript could also be written in Elm. This led to projects like take-home, the only full stack Elm application.\n\nIn this talk, we'll examine some of the things I've heard are \"impossible\" in Elm, such as: animation, inline-styles, writing CLI tools, writing a debugger, writing a server. We'll also discuss why some of these ideas are better off as unpublished packages, and the general problem-solving approach I take to making the unsolvable solved."
         , speaker = noah
-        , time = ( "11:55", "12:15" )
+        , time = ( "12:00", "12:20" )
         }
-    , NonTalk "Break" ( "12:15", "12:35" )
+    , NonTalk "Break" ( "12:20", "12:40" )
     , TalkEntry
         { title = "Localization for Elm as a pre-build phase"
         , abstract = "Localization is usually done by providing a global dictionary function, passing the language to each view and keeping the current language in the app's model. This makes it difficult to add a second language to an existing code base and adds boiler plate code when 90% of the users will only care for one of the supported languages. Our concept is to have a small pre-build-phase to generate multiple single-language elm apps from the same code base."
         , speaker = felix
-        , time = ( "12:35", "12:55" )
+        , time = ( "12:40", "13:00" )
         }
     , TalkEntry
         { title = "An Elm Kernel for Jupyter"
         , abstract = "I'll introduce the Elm kernel for Jupyter notebooks. Jupyter (a.k.a. IPython) notebooks are a way to interleave text and executable code, and this new kernel allows you to embed Elm code in them. We'll look at how the kernel is implemented, I'll demonstrate how to use it, and hopefully participants will be inspired to use it or even contribute to the project. You can get the code for the project at github.com/abingham/jupyter-elm-kernel"
         , speaker = austin
-        , time = ( "12:55", "13:15" )
+        , time = ( "13:00", "13:20" )
         }
-    , NonTalk "Lunch" ( "13:15", "14:00" )
+    , NonTalk "Lunch" ( "13:20", "14:10" )
     , TalkEntry
         { title = "Putting the Elm Platform in the Browser"
         , abstract = "I wanted it to be easier for folks to try the Elm platform and share it with each other, so I made Ellie and put it in the browser! Since its release in January of this year people have shared about 5000 revisions, but this is only the beginning. I want you all to share in my appreciation for this community and my desire for it to grow, so in this talk I'm going to share the life story of Ellie. You'll feel like you were there on the day it was first released, and share my own excitement about where it's heading now and what comes next."
         , speaker = luke
-        , time = ( "14:00", "14:40" )
+        , time = ( "14:10", "14:50" )
         }
-    , NonTalk "Break" ( "14:40", "15:00" )
+    , NonTalk "Break" ( "14:50", "15:10" )
     , TalkEntry
         { title = "Move Fast and Don’t Break Things. Running a Startup on Elm"
         , abstract = "Are you concidering using Elm at your new fancy startup ? I did just this a year ago and now I'm coming to Oslo\nto talk about this \"crazy\" (read: great) decision and how it turned out. Was it a good experience ? Would I do the same again ? And – is Elm as good as they say ? Get the answer to these questions and many more in this talk."
         , speaker = nils
-        , time = ( "15:00", "15:20" )
+        , time = ( "15:10", "15:30" )
         }
     , TalkEntry
         { title = "Bootstrapping a project with Elm"
         , abstract = "At Blank we recently did a greenfield project that we knew would be frontend-heavy and whose a design and API would probably change many times, as we learned more. As if that wan't enough changes, the team would also see some rotation throughout the project.\nI will talk about why we thought Elm would be a good choice given the nature of the project, but most of all about our actual experience of using Elm with a team that, with one exception, had not used Elm before."
         , speaker = erlend
-        , time = ( "15:20", "15:40" )
+        , time = ( "15:30", "15:50" )
         }
-    , NonTalk "Break" ( "15:40", "16:00" )
+    , NonTalk "Break" ( "15:50", "16:10" )
     , TalkEntry
         { title = "The Design Evolution of elm-css and elm-test"
         , abstract = "I've been fortunate to participate in the design of two popular projects, elm-css and elm-test. In the course of iterating on their designs with collaborators and community members, I've learned a lot about what worked well and what didn't. This talk will dive into the design decisions that shaped the past, present, and future of elm-css and elm-test. How did these projects balance build-time guarantees, performance, and user experience? What short-term sacrifices were deemed acceptable? What long-term invariants were considered worth preserving even under pressure? What's next for these libraries, and why? Come find out!"
         , speaker = richard
-        , time = ( "16:00", "16:40" )
+        , time = ( "16:10", "16:50" )
         }
     , NonTalk "Break" ( "17:00", "17:30" )
     , NonTalk "Dinner" ( "17:30", "19:00" )
