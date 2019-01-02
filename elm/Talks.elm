5f46d669b72e762adb90687d59177638b698f675
Ingar Almklov <ingara@gmail.com>
2019-01-02 16:13:30 +0100

    Add abstracts: Tessa, Jonas, Viktor

--- elm/Talks.elm
+++ elm/Talks.elm
@@ -63,7 +63,12 @@ all =
       , time = ( "", "" )
       }
     , { title = "On the Elm Track with NSB"
-      , abstract = ""
+      , abstract = """In 2017 NSB, one of Norways largest providers of public transport,
+faced a situation where large and continuous changes to their young but already dated web platform were needed.The choice was made to move the code base to Elm, and now, one and a half year later, all the most critical parts of the platform is built in Elm.
+
+In this talk I will share our team's experience from this process. I will talk about why we ended up building many medium sized applications instead of a large one and how we have created private libraries and a view-function library to reuse code between applications, boost our productivity and make the applications better.
+
+Along the way I will share which things we have found to be simple thanks to Elm, and the pain points we have encountered."""
       , speaker = Speakers.jonas
       , time = ( "", "" )
       }
@@ -78,7 +83,9 @@ all =
       , time = ( "", "" )
       }
     , { title = "Automated conference scheduling powered by Elm"
-      , abstract = ""
+      , abstract = """Preparing an agenda for a big conference with many sessions and parallel tracks is extremely hard to do manually. NoConf is an automated agenda scheduler, which uses AI techniques to solve this problem. It has a frontend implemented in Elm and in this talk we will see how the decision to use Elm was a crucial factor of the transition from a small side project of a complete newbie in web programming into a real world application.
+
+We will take a look at Elm’s learning curve. How it’s like to not have a JS background, or any web programming related knowledge for that matter, and still being able to face the challenge. Which pain points are most significant in this case and which advantages a non-web developer is getting from the start. How problems and solutions evolve along with project development and gained experience in Elm."""
       , speaker = Speakers.viktor
       , time = ( "", "" )
       }
@@ -103,7 +110,7 @@ all =
       , time = ( "", "" )
       }
     , { title = "Color Coding with Elm"
-      , abstract = ""
+      , abstract = "As we build out user interfaces, we make choices about color that involve us in a vivid history of experimentation and exploration. We benefit from the bountiful attention that painters, designers, physicists, biologists, doctors, mathematicians, makeup artists, and so many others have paid to studying color and its perception. Within our field as software engineers, we can be confident in the strengths of the color spaces we've defined, like RGB and HSL. However, we may also find that these color spaces, while they perfectly describe how we want our colors to render, don't actually reflect what we want our users to see. In this talk, we'll turn our gaze towards our users' perception of colors: how can we make palette choices that work for our users? My hope is that you leave this talk feeling excited about colors, and maybe even like your attitude toward them just slipped on a pair of rosy glasses."
       , speaker = Speakers.tessa
       , time = ( "", "" )
       }
